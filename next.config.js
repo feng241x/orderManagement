65d77d1ae810065413009985454c701f32b933c1
ISHANGSF-PC\Administrator <feng241x@qq.com>
2023-04-12 21:43:41 +0800

    前端打包端口号设置为80

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  server: {
+    port: 80, // 修改端口号为所需的值
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
